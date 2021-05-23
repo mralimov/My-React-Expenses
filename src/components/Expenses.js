29a5c68f3e1457af8f1c498c1d9f3d973cb5baa8
mralimov <mralimov89@gmail.com>
2021-05-23 00:12:27 -0400

    Expenses refactoring to Card shell wrapper

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,30 +1,30 @@
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
-
+import Card from './UI/Card';
 function Expenses(props) {
   return (
-    <>
+    <Card className="expenses">
       <ExpenseItem
-        title={props[0].title}
-        amount={props[0].amount}
-        date={props[0].date}
+        title={props.data[0].title}
+        amount={props.data[0].amount}
+        date={props.data[0].date}
       ></ExpenseItem>
       <ExpenseItem
-        title={props[1].title}
-        amount={props[1].amount}
-        date={props[1].date}
+        title={props.data[1].title}
+        amount={props.data[1].amount}
+        date={props.data[1].date}
       ></ExpenseItem>
       <ExpenseItem
-        title={props[2].title}
-        amount={props[2].amount}
-        date={props[2].date}
+        title={props.data[2].title}
+        amount={props.data[2].amount}
+        date={props.data[2].date}
       ></ExpenseItem>
       <ExpenseItem
-        title={props[3].title}
-        amount={props[3].amount}
-        date={props[3].date}
+        title={props.data[3].title}
+        amount={props.data[3].amount}
+        date={props.data[3].date}
       ></ExpenseItem>
-    </>
+    </Card>
   );
 }
 export default Expenses;
