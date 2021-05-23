af4ba90ad42d7723c974a30b67a99e70857fc0af
mralimov <mralimov89@gmail.com>
2021-05-23 00:07:51 -0400

    refactoring Expenses.js to ExpensesItem

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -2,5 +2,29 @@ import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 
 function Expenses(props) {
-  return <ExpenseItem data={props} />;
+  return (
+    <>
+      <ExpenseItem
+        title={props[0].title}
+        amount={props[0].amount}
+        date={props[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props[1].title}
+        amount={props[1].amount}
+        date={props[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props[2].title}
+        amount={props[2].amount}
+        date={props[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props[3].title}
+        amount={props[3].amount}
+        date={props[3].date}
+      ></ExpenseItem>
+    </>
+  );
 }
+export default Expenses;
