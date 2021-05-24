0f5d62aaf9122588babcaa7f54874052e9857f01
mralimov <mralimov89@gmail.com>
2021-05-24 14:28:45 -0400

    importing NewExpense Form component to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
-
+import NewExpenses from './components/NewExpenses/NewExpenses';
 function App() {
   const expenses = [
     {
@@ -25,6 +25,7 @@ function App() {
   return (
     <div>
       <h2>My React Expenses Component!</h2>
+      <NewExpenses />
       <Expenses data={expenses} />
     </div>
   );
