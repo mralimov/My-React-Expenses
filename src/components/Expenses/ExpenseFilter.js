b66af0d38437c8a35c65a12d5d4615bdb100eaa3
mralimov <mralimov89@gmail.com>
2021-05-24 18:15:30 -0400

    passing selected year to Parent el

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -1,10 +1,13 @@
 import './ExpenseFilter.css';
 const ExpenseFilter = props => {
+  const yearFilterChoice = e => {
+    props.onFilterredYear(e.target.value);
+  };
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select>
+        <select onChange={yearFilterChoice}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
