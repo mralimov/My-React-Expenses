97eb0577619321451e8e52223dd3e26b01201f23
mralimov <mralimov89@gmail.com>
2021-05-24 12:33:15 -0400

    NewExpenses component

---
+++ src/components/NewExpenses/NewExpenses.js
@@ -0,0 +1,12 @@
+import ExpenseForm from './ExpensesForm';
+import './NewExpenses.css';
+
+const NewExpenses = () => {
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
+    </div>
+  );
+};
+
+export default NewExpenses;
