03035449864e73bcb19b5d3fa91924f0b4a4ca95
mralimov <mralimov89@gmail.com>
2021-05-24 15:46:04 -0400

    Passing data from child to parent el.

--- src/components/NewExpenses/NewExpenses.js
+++ src/components/NewExpenses/NewExpenses.js
@@ -1,10 +1,17 @@
 import ExpenseForm from './ExpensesForm';
 import './NewExpenses.css';
 
-const NewExpenses = () => {
+const NewExpenses = props => {
+  const addNewExpenseHandler = newExpense => {
+    const newExpenseFunc = {
+      ...newExpense,
+      id: Math.random().toString(),
+    };
+    props.onNewExpense(newExpenseFunc);
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm></ExpenseForm>
+      <ExpenseForm onSaveNewExpense={addNewExpenseHandler}></ExpenseForm>
     </div>
   );
 };
