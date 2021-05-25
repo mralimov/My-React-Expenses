d8a90ee57eabd305fd9b8b78a0c8fe5abb4f612a
mralimov <mralimov89@gmail.com>
2021-05-25 00:35:25 -0400

    Refactoring ExpenseItem rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,26 +12,14 @@ function Expenses(props) {
     <div>
       <Card className="expenses">
         <ExpenseFilter selected={year} onFilterredYear={choosenYearHandler} />
-        <ExpenseItem
-          title={props.data[0].title}
-          amount={props.data[0].amount}
-          date={props.data[0].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.data[1].title}
-          amount={props.data[1].amount}
-          date={props.data[1].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.data[2].title}
-          amount={props.data[2].amount}
-          date={props.data[2].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.data[3].title}
-          amount={props.data[3].amount}
-          date={props.data[3].date}
-        ></ExpenseItem>
+        {props.data.map(expense => (
+          <ExpenseItem
+            key={props.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
