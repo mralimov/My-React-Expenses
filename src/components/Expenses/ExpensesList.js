addc205b3e883093be1089c4f317c85d5e91d958
mralimov <mralimov89@gmail.com>
2021-05-25 10:36:24 -0400

    ExpensesList component

---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,22 @@
+import ExpenseItem from './ExpenseItem';
+import './ExpensesList.css';
+
+const ExpensesList = props => {
+  if (props.transactions.length === 0) {
+    return <h2 className="expenses-list__fallback">No Expenses Found.</h2>;
+  }
+  return (
+    <ul className="expense-list">
+      {props.transactions.map(expense => (
+        <ExpenseItem
+          id={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ExpensesList;
