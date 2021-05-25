4dfe24802c6621e7d88628ea54f0e7028176e4aa
mralimov <mralimov89@gmail.com>
2021-05-25 00:35:03 -0400

    Refactoring Sample expenses and state

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import Expenses from './components/Expenses/Expenses';
 import NewExpenses from './components/NewExpenses/NewExpenses';
+import React, { useState } from 'react';
+const SAMPLE_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450.09,
+    date: new Date(2021, 5, 12),
+  },
+];
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450.09,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(SAMPLE_EXPENSES);
 
-  const addNewExpenseHandler = el => {
-    console.log('I am from APP.JS');
-    console.log(el);
+  const addNewExpenseHandler = expense => {
+    setExpenses(prevExpenses => {
+      return [expense, ...SAMPLE_EXPENSES];
+    });
   };
   return (
     <div>
