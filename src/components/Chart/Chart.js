0c24cdcdf8769daca56efa556da6ec6c02d3a3d9
mralimov <mralimov89@gmail.com>
2021-05-25 15:57:14 -0400

    Chart Component

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,7 +1,18 @@
 import ChartBar from './ChartBar';
 import './Chart.css';
-const Chart = () => {
-  return <div className="chart"></div>;
+const Chart = props => {
+  return (
+    <div className="chart">
+      {props.dataPoints.map(dataPoint => (
+        <ChartBar
+          value={dataPoint.value}
+          key={dataPoint.label}
+          maxValue={null}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Chart;
